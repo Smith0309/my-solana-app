e4cf871f6b3ac958369cbec27ed78b283cc0f162
smile930307 <ImamEugenaU2K@gmail.com>
2022-08-17 00:51:14 +0900

    update

--- programs/mysolanaapp/src/lib.rs
+++ programs/mysolanaapp/src/lib.rs
@@ -6,12 +6,16 @@ declare_id!("Fg6PaFpoGXkYsidMpWTK6W2BeZ7FEfcYkg476zPFsLnS");
 pub mod mysolanaapp {
     use super::*;
 
-    pub fn setup
+    pub fn setup_platform(ctx : Context<>) {
+        
+    }
 
     pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
         Ok(())
     }
 }
+
+#[account]
 
 #[derive(Accounts)]
 pub struct Initialize {}
